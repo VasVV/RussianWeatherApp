b45372309301ea527fed796474ff23300a953732
VasVV <radioheadboy0@gmail.com>
2021-12-31 12:59:42 +0300

    commit

---
+++ weathermapru/README.md
@@ -0,0 +1,6 @@
+# Russian Weather Map App
+# Installation
+1) Download repository
+2) cd to folder
+3) npm i
+4) npm run dev
---
+++ weathermapru/server/index.js
@@ -0,0 +1,48 @@
+
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bp = require('body-parser');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+
+const app = express();
+
+
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+const headers = {
+    'X-Yandex-API-Key': process.env.API_KEY,
+  }
+
+app.post('/recieveweather', (req, res) => {
+    
+    let lat = req.body.lat;
+    let lon = req.body.lon;
+    
+
+    const requestOptions = {
+    method: 'GET',
+    headers,
+    redirect: 'follow'
+    };
+
+    fetch(`https://api.weather.yandex.ru/v2/forecast?lat=${lat}&lon=${lon}&extra=true`, requestOptions)
+    .then(response => response.text())
+    .then(result => res.send(result))
+    .catch(error => console.log('error', error));
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
